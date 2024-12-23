6f6a924576c82bc9d186708039843822e178c32b
22316708@student.ciu.edu.tr <amexabee@yahoo.com>
2024-12-23 13:19:58 +0200

    Update query

--- sql/queries/1.sql
+++ sql/queries/1.sql
@@ -4,6 +4,8 @@ SELECT first_name || ' ' || last_name name
 FROM "Person" p
 JOIN "Customer" c 
     ON c.person_id = p.person_id
+JOIN "Employee" e 
+    ON e.person_id = c.created_by_id
 JOIN "Branch" b 
-    ON b.branch_id = c.created_by_id
+    ON b.branch_id = e.branch_id
 WHERE b.branch_city <> 'New York';
